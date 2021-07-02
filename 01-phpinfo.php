82a24d21ef8be9802bce706e540703ff1cc86a40
mikhawa <michaeljpitz@gmail.com>
2021-07-02 14:19:17 +0200

    phpinfo

---
+++ 01-phpinfo.php
@@ -0,0 +1,5 @@
+<?php
+
+// fonction native de PHP, nous affiche les versions, bibliothèques et réglages de PHP (utile au changement de serveur)
+// https://www.php.net/manual/fr/function.phpinfo
+phpinfo();
