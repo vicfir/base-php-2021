4abc2a2a2028ebd0b97a9ac1f9b1e2829281c506
mikhawa <michaeljpitz@gmail.com>
2021-07-09 11:59:53 +0200

    09 5

--- 09-exemple-db.php
+++ 09-exemple-db.php
@@ -1,6 +1,17 @@
 <?php
 // $_POST existe par défaut en tant que tableau, lorsqu'on récupère un formulaire, il devient un tableau associatif contenant les valeurs
 // var_dump($_POST);
+
+// connexion à la DB
+$connexionDB = mysqli_connect("localhost", "root", "", "exe9", 3307);
+
+// récupération des commentaires de la table goldenbook
+$recupComments = mysqli_query($connexionDB, "SELECT * FROM goldenbook ORDER BY thedate DESC;");
+
+// on va compter le nombre de commentaires récupérés
+$nbComment = mysqli_num_rows($recupComments);
+
+// transformation des données
 ?>
 <html>
     <head>
@@ -13,7 +24,7 @@
             <textarea name="thetext" placeholder="Votre texte" required></textarea><br> 
             <input type='submit' value='Envoyer'/><br>
         </form>
-        <h3>Nous avons x commentaires</h3>
+        <h3>Nous avons <?=$nbComment?> commentaires</h3>
         <h4>Le nom</h4>
         <p>écrit</p>
         <p>Le ...date</p><hr>
