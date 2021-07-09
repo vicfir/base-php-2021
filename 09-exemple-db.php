8a32bc29fdf05cc00e0f1df24d3fe2f6031f49d9
mikhawa <michaeljpitz@gmail.com>
2021-07-09 12:20:20 +0200

    09 mysqli présentation

--- 09-exemple-db.php
+++ 09-exemple-db.php
@@ -2,8 +2,28 @@
 // $_POST existe par défaut en tant que tableau, lorsqu'on récupère un formulaire, il devient un tableau associatif contenant les valeurs
 // var_dump($_POST);
 
+
 // connexion à la DB
 $connexionDB = mysqli_connect("localhost", "root", "", "exe9", 3307);
+
+// communication entre php et mysql en utf8
+mysqli_set_charset($connexionDB, "utf8");
+
+// si le formulaire est envoyé
+if(!empty($_POST)){ // non vide
+    // sécurité minimale des variables post vers la locale
+    $thetext = htmlspecialchars(strip_tags(trim($_POST['thetext'])),ENT_QUOTES);
+    $thename = htmlspecialchars(strip_tags(trim($_POST['thename'])),ENT_QUOTES);
+    
+    // si les variables ne sont pas vides
+    if(!empty($thename)&&!empty($thetext)){
+        // insertion dans la DB
+        mysqli_query($connexionDB, "INSERT INTO goldenbook (thename,thetext) 
+                                    values ('$thename','$thetext')");
+        
+    }
+}
+
 
 // récupération des commentaires de la table goldenbook
 $recupComments = mysqli_query($connexionDB, "SELECT * FROM goldenbook ORDER BY thedate DESC;");
@@ -11,11 +31,13 @@ $recupComments = mysqli_query($connexionDB, "SELECT * FROM goldenbook ORDER BY t
 // on va compter le nombre de commentaires récupérés
 $nbComment = mysqli_num_rows($recupComments);
 
-// transformation des données
+// transformation des données en tableau associatif
+$datasComment = mysqli_fetch_all($recupComments, MYSQLI_ASSOC);
 ?>
 <html>
     <head>
         <title>Livre d'or sur une page</title>
+        <meta charset="utf-8">
     </head>
     <body>
         <h1>Livre d'or sur une page</h1>
@@ -25,9 +47,16 @@ $nbComment = mysqli_num_rows($recupComments);
             <input type='submit' value='Envoyer'/><br>
         </form>
         <h3>Nous avons <?=$nbComment?> commentaires</h3>
-        <h4>Le nom</h4>
-        <p>écrit</p>
-        <p>Le ...date</p><hr>
+        <?php
+        // var_dump($datasComment);
+        foreach($datasComment as $item):
+        ?>
+        <h4><?=$item['thename']?></h4>
+        <p><?=$item['thetext']?></p>
+        <p>Le <?=$item['thedate']?></p><hr>
+        <?php
+        endforeach;
+        ?>
     </body>
 </html>
 
