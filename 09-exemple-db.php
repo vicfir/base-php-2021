0729c944781d3979459fa76d4fceeb8cd71812e7
mikhawa <michaeljpitz@gmail.com>
2021-07-09 11:24:57 +0200

    09 2

--- 09-exemple-db.php
+++ 09-exemple-db.php
@@ -5,7 +5,9 @@
     <body>
         <h1>Livre d'or sur une page</h1>
         <form name="goldenbook" method="post" action="">
-            
+            <input type="text" name="thename" placeholder="Votre nom" required /><br> 
+            <textarea name="thetext" placeholder="Votre texte" required></textarea><br> 
+            <input type='submit' value='Envoyer'/><br>
         </form>
     </body>
 </html>
