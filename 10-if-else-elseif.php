1a3f92d25349f3e4d30278f270a24019bec337e0
mikhawa <michaeljpitz@gmail.com>
2021-09-03 11:58:20 +0200

    if elseif else

--- 10-if-else-elseif.php
+++ 10-if-else-elseif.php
@@ -39,12 +39,60 @@
     if($nb1>0 || $nb2>0){
         echo '<p>au moins 1 des 2 (ou les deux) $nb1 ('.$nb1.') ou $nb2 ('.$nb2.') sont strictement positifs</p>';
     }
-    echo "<h4>OU exclusif (ou bien) - XOR - ^</h4>";
 
+    echo "<h4>OU exclusif (ou bien) - XOR - ^</h4>";
+    /* SI ($nb1 est plus grand que 0 OU BIEN que $nb2 est plus grand que 0) => 
+    OU exclusif : 1 seule des 2 conditions doit être true
+    (true ^ true) = > false 
+    (true ^ false) => true 
+    (false ^ false) => false
+    */
     if($nb1>0 ^ $nb2>0){
         echo '<p>Ne s\'affiche que si 1 seul des numériques est positif (ou exclusif): $nb1 ('.$nb1.') - $nb2 ('.$nb2.')</p>';
     }
 
+    ?>
+    <h3>ELSE</h3>
+    <p>S'exécute si le code entre les parenthèses du IF nous donne un false (global), si c'est le cas, exécution du code entre { }. </p>
+
+    <?php
+    // Si ($nb1 est plus grand > que $nb2) => exécution du code entre {}
+    if($nb1 > $nb2){
+        echo '<p>$nb1 ('.$nb1.') est plus grand que $nb2 ('.$nb2.') - IF</p>';
+    }else{
+        echo '<p>$nb1 ('.$nb1.') n\'est pas plus grand que $nb2 ('.$nb2.') - ELSE</p>';
+    }
+
+    echo "<h4>inverser - NOT - IS NOT - !</h4>";
+
+    // version longue si on sait qu'on aura pas de pagination ici
+    if(isset($pagination)){
+
+    }else{
+        echo"<p>page 1</p>"; 
+    }
+
+    /* pour avoir le else sans le if - le ! inverse le résultat d'une condition:
+        !true => false
+        !false => true
+        */
+    
+    if(!isset($pagination)){
+        echo"<p>page 1</p>"; 
+    }
+    ?>
+    <h3>ELSE IF - ELSEIF</h3>
+    <p>S'exécute si le code entre les parenthèses du IF nous donne un false (global), si c'est le cas revérification de la nouvelle condition et si true exécution du code entre { }, sinon passage au elseif suivant ou au else si existant. </p>
+    <?php
+    if($nb1>0 && $nb2>0){
+        echo "<p>$nb1 et $nb2 sont des nombres positifs</p>";
+    }elseif($nb1>0){
+        echo "<p>Seul $nb1 est un nombre positif</p>";
+    }else if(($nb2/2) == round($nb2/2)){
+        echo "<p>$nb2 est pair</p>";
+    }else{
+        echo "<p>$nb2 est impair</p>";
+    }
     ?>
 </body>
 </html>
