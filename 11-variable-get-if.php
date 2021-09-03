402b15de4daddd354e1158de0a262d5a4e879a01
mikhawa <michaeljpitz@gmail.com>
2021-09-03 12:20:36 +0200

    exe 11 elseif else ou switch

---
+++ 11-variable-get-if.php
@@ -0,0 +1,47 @@
+<?php
+// variable de contenu
+$pages = [
+    1 => [
+        "titre" => "Page d'accueil",
+        "texte" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis incidunt voluptas, rerum, veniam soluta aliquam qui quae quia sunt laudantium excepturi exercitationem esse recusandae nisi unde! Nam placeat nostrum mollitia.",
+    ],
+    2 => [
+        "titre" => "Contact",
+        "texte" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, dignissimos ullam, voluptates, molestiae laboriosam quidem culpa ab deleniti ea eum eius? Natus ab saepe tempore non sequi unde amet eveniet!",
+    ],
+    3 => [
+        "titre" => "Achats",
+        "texte" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum quo blanditiis, pariatur optio sapiente iure aliquid cumque minima at laudantium nulla quisquam quas doloremque culpa odio soluta enim provident eum.",
+    ],
+
+];
+
+if(isset($_GET['idpage'])){
+
+if($_GET['idpage']==1){
+    $title = $pages[1]["titre"];
+    $texte = $pages[1]["texte"];
+} // ... sinon si
+
+// page d'accueil par défaut    
+}else{
+    $title = $pages[1]["titre"];
+    $texte = $pages[1]["texte"];
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variables Get et contenu différent</title>
+</head>
+<body>
+    <h1>Variables Get et contenu différent</h1>
+    <nav><a href="?idpage=1">Accueil</a> - <a href="?idpage=2">Contact</a> - <a href="?idpage=3">Achats</a> -</nav>
+    <h2><?=$title?></h2>
+    <p><?=$texte?></p>
+</body>
+</html>
