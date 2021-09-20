76b57cc7c63762a7b5fa7f69470247d0bfb4fdbe
mikhawa <michaeljpitz@gmail.com>
2021-09-20 14:10:12 +0200

    19 exe ok

--- 19-functions.php
+++ 19-functions.php
@@ -1,5 +1,14 @@
 <?php
 
+
+/**
+ * calcul
+ *
+ * @param  mixed $num1
+ * @param  mixed $num2
+ * @param  mixed $opertor
+ * @return void
+ */
 function calcul($num1, $num2, $opertor="+" ){
 
     // variable de sortie
@@ -9,23 +18,29 @@ function calcul($num1, $num2, $opertor="+" ){
     $n1 = (float) $num1;
     $n2 = (float) $num2;
 
+
     switch($opertor){
         case "+": 
             $out.= "$n1 + $n2 = ".($n1+$n2);
             break;
         case "-";
-
+            $out.= "$n1 - $n2 = ".($n1-$n2);
             break;
         case "X";
-
+            $out.= "$n1 X $n2 = ".($n1*$n2);
             break;
         case "/";
             // condition pour empêcher la division par 0 avec affichage d'erreur personalisée
+            if($n2==0){
+                $out.="La division par 0 est interdite";
+            }else{
+                $out.= "$n1 / $n2 = ".($n1/$n2);
+            }
         break;
         default: 
             // redirection si chipotage de formulaire
             header("Location: https://www.facebook.com/LeSoWhat/");
-            exit();
+            exit(); // pour la compatibilité avec certains serveurs, on arrête le script
     }
 
     return $out;
