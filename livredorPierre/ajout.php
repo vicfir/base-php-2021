4514a744c2c9e824f23a454ff14d5075f08a90f7
mikhawa <michaeljpitz@gmail.com>
2021-09-24 16:34:55 +0200

    ajout.php

--- livredorPierre/ajout.php
+++ livredorPierre/ajout.php
@@ -1,6 +1,35 @@
 <?php 
 // chargement du fichier de configuration, le require_once ne permet pas l'erreur (exit immédiat) et surtout ne charge le fichier qu'une seule fois !!! (sinon erreur des constantes redéfinies)
 require_once "config.php";
+
+// debug post
+var_dump($_POST);
+
+// si le formulaire a été envoyé => si la superglobale $_POST n'est pas vide
+if(!empty($_POST)){
+    // récupération en variables locales des variables $_POST
+
+    // le pseudo
+    $pseudo = $_POST['pseudo'];
+    
+    // on retire les espaces vides devant et derrière la chaîne
+    $pseudo = trim($pseudo);
+    
+    // on retire toutes les balises html, javascript etc... 
+    $pseudo = strip_tags($pseudo);
+    
+    // on va encoder en html tous les caractères spéciaux qui pourraient poser problèmes, on utilise ENT_QUOTES comme flag, pour convertir les " et ' guillemets
+    $pseudo = htmlspecialchars($pseudo,ENT_QUOTES);
+
+    // le mail , on utilise un filtre de variable tout fait (renvoie l'adresse si true, false si false)
+    $mail = filter_var($_POST['mail'],FILTER_VALIDATE_EMAIL);
+
+    // le message
+    $message = htmlspecialchars(strip_tags(trim($_POST['msg'])),ENT_QUOTES);
+
+
+}
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -28,18 +57,18 @@ require_once "config.php";
         VALUES('mon pseudo','mon email','mon message',NOW());
         -->
         <main>
-            <form method="POST" action="ajout.php">
+            <form method="POST" action="">
                 <div class="nom">
                 <label for="pseudo">Pseudo</label>
-                <input type="text" name="pseudo" id="pseudo" maxlength="30" placeholder="Entrez votre pseudo (max 30 caractères)" />
+                <input type="text" name="pseudo" id="pseudo" maxlength="30" placeholder="Entrez votre pseudo (max 30 caractères)" required />
                 </div>
                 <div class="email">
                 <label for="email">E-mail</label>
-                <input type="email" name="email" id="email" maxlength="50" placeholder="Entrez votre e-mail" />
+                <input type="email" name="email" id="email" maxlength="50" placeholder="Entrez votre e-mail" required />
                 </div>
                 <div class="message">
                 <label for="msg">Message</label>
-                <textarea name="msg" id="msg" placeholder="Entrez ici votre message"></textarea>
+                <textarea name="msg" id="msg" placeholder="Entrez ici votre message" required></textarea>
                 </div>
                 <div class="envoi">
                 <input type="submit" value="Envoyer ce message" />
