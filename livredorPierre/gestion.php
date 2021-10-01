ffa35047da975bebe4df47b20400314625ba2856
mikhawa <michaeljpitz@gmail.com>
2021-10-01 09:32:27 +0200

    gestion.php select request

--- livredorPierre/gestion.php
+++ livredorPierre/gestion.php
@@ -1,6 +1,38 @@
 <?php 
 // chargement du fichier de configuration, le require_once ne permet pas l'erreur (exit immédiat) et surtout ne charge le fichier qu'une seule fois !!! (sinon erreur des constantes redéfinies)
 require_once "config.php";
+
+// connexion en mysqli
+$connectDB = @mysqli_connect(DB_HOST, DB_USER, DB_PWD, DB_NAME, DB_PORT);
+
+// ceci est un gestionnaire d'erreur
+if (!$connectDB) {
+    die("Problème lors de la connexion :" . mysqli_connect_error());
+}
+
+// si on est ici, c'est qu'on a pu se connecter ! (le die() n'a pas été enclenché)
+
+// on change le charset de communication entre le serveur sql et le serveur php
+mysqli_set_charset($connectDB,DB_CHARSET);
+
+// création d'une variable contenant notre requête sql (en SQL donc!)
+$sql = "SELECT *
+FROM messages
+ORDER BY date_msg DESC;";
+
+// on effectue la requête SQL avec mysqli_query($varDeConnexion,"DU SQL")
+$requestDB = mysqli_query($connectDB, $sql) or die("Problème lors de la requête de sélection des messages: " . mysqli_error($connectDB));
+
+// on récupère le nombre de ligne de résultats avec mysqli_num_rows($requete)
+$nbMessage = mysqli_num_rows($requestDB);
+
+// si on a au moins un message (0=> false, 1 ou plus => true)
+if($nbMessage){
+    // si on a un message OU plusieurs messages, on va toujours utiliser les mysqli_fetch_all avec le flag: MYSQLI_ASSOC - mysqli_fetch_all crée TOUJOURS un tableau indexé, CONTENANT le type de variables choisis via le flag (constantes MYSQLI_ASSOC)
+    $messages = mysqli_fetch_all($requestDB,MYSQLI_ASSOC);
+    var_dump($messages);
+}
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -51,24 +83,7 @@ require_once "config.php";
                     <td>08-09-2021 11:32:51</td>
                     <td>Supprimer</td>
                 </tr>
-                <tr>
-                    <td>2</td>
-                    <td>Michaël</td>
-                    <td>michael.pitz@cf2m.be</td>
-                    <td>Salut lulu !
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit optio, non veritatis praesentium accusamus dolor tempora, ea reprehenderit nobis incidunt, impedit dolores. Eius, atque corrupti labore possimus sed quidem sequi.</td>
-                    <td>08-09-2021 11:15:25</td>
-                    <td>Supprimer</td>
-                </tr>
-                <tr>
-                    <td>1</td>
-                    <td>Pierre</td>
-                    <td>pierre.sandron@cf2m.be</td>
-                    <td>Bonjour les amis !
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi eligendi quibusdam omnis voluptates, natus molestiae totam eius ratione, fugiat voluptatum esse voluptatibus nostrum. Quisquam odio culpa, voluptates velit id soluta.</td>
-                    <td>08-09-2021 11:06:45</td>
-                    <td>Supprimer</td>
-                </tr>
+                
             </table>
         </main>
         <footer>        
