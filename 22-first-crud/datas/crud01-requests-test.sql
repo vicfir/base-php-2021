421c3fc69a2c74820a6c135d28d158302452f28b
mikhawa <michaeljpitz@gmail.com>
2021-10-04 16:31:32 +0200

    crud01-requests-test.sql

---
+++ 22-first-crud/datas/crud01-requests-test.sql
@@ -0,0 +1,7 @@
+# tests de nos requêtes
+
+# Sélection de tous les champs de la table thearticle.
+SELECT * FROM thearticle;
+
+# Sélection de tous les champs de la table theuser.
+SELECT * FROM theuser;
