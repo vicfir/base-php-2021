b4a4939e257ac6ba01aaf06a5af59cb5072a1280
mikhawa <michaeljpitz@gmail.com>
2021-10-08 10:11:21 +0200

    erreur404.php

--- 22-first-crud/config.php
+++ 22-first-crud/config.php
@@ -14,5 +14,5 @@ define("MDB_CHARSET","utf8");
 define("NEWS_HOME_LENGTH", 220); // nombre de caractères du texte sur la page d'accueil
 define("NEWS_BR",true); // si true, retours automatiques à la ligne, si false pas de <br>
 
-// on définit les pages publiques acceptées en variable get
+// on définit les pages publiques acceptées en variable get dans une constante contenant un tableau (depuis PHP7)
 define("PUBLIC_PAGE", ['home','user','article']);
