14baef5b02681a8a7e4b89ac34899306a4a4d4d9
mikhawa <michaeljpitz@gmail.com>
2021-10-08 10:49:35 +0200

    request in home.php

--- 22-first-crud/public_page/home.php
+++ 22-first-crud/public_page/home.php
@@ -1,5 +1,20 @@
 <?php
+/*
+Page d'accueil, doit passer par index.php (CF)
+*/
 
+// on va récupérer tous les articles avec le texte à maximum 220 caractères, avec l'utilisateur qui l'a écrit (voir datas/crud01-requests-test.sql)
+$sql="SELECT a.idthearticle, a.thearticletitle, SUBSTR(a.thearticletext,1,250) AS thearticletext, a.thearticledate,
+             u.idtheuser, u.theuserlogin
+    FROM thearticle a
+        INNER JOIN  theuser u 
+            ON u.idtheuser = a.theuser_idtheuser
+    ORDER BY a.thearticledate DESC;";
+
+// on effectue la requête
+$recup = mysqli_query($db,$sql) or die("Erreur lors de la requête :".mysqli_error($db));
+
+//
 ?>
 <!DOCTYPE html>
 <html lang="en">
