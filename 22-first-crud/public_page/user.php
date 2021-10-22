3bf22d12ad376c033503f6d603984dd3b5cbfbfc
mikhawa <michaeljpitz@gmail.com>
2021-10-22 12:20:56 +0200

    print_r

--- 22-first-crud/public_page/user.php
+++ 22-first-crud/public_page/user.php
@@ -7,7 +7,30 @@ if(isset($_GET['id']) &&
     // transtypage de string to int
     $iduser = (int) $_GET['id'];
 
-    $sql="";
+    // requête préparée dans workbench
+    $sql="SELECT u.idtheuser, u.theuserlogin,
+    GROUP_CONCAT(a.idthearticle ORDER BY a.thearticledate DESC) AS idthearticle, 
+    GROUP_CONCAT(a.thearticletitle ORDER BY a.thearticledate DESC SEPARATOR '|||') AS thearticletitle,
+    GROUP_CONCAT(SUBSTR(a.thearticletext,1,150) ORDER BY a.thearticledate DESC SEPARATOR '|||') AS thearticletext, 
+    GROUP_CONCAT(a.thearticledate ORDER BY a.thearticledate DESC SEPARATOR '|||') AS thearticledate
+        FROM theuser u
+            LEFT JOIN thearticle a
+            ON u.idtheuser = a.theuser_idtheuser
+        WHERE u.idtheuser = $iduser
+    GROUP BY u.idtheuser
+    ;";
+
+    // exécution de la requête
+    $request = mysqli_query($db,$sql) or die("Erreur lors de la requête avec jointures : ".mysqli_error($db));
+
+    // si on a un résultat (ça a fonctionné 0 ou 1)
+    if(mysqli_num_rows($request)){
+        $result = mysqli_fetch_assoc($request);
+    }else{
+        // redirection
+    header("Location: ./");
+    die;
+    }
 
 // pas de variable id    
 }else{
@@ -29,6 +52,9 @@ if(isset($_GET['id']) &&
     // menu publique
     include "menu.php";
     ?>
+    <pre>
+        <?php print_r($result) ?>
+    </pre>
     <h1>First CRUD | User | <!-- ici le login de l'utilisateur --></h1>
 </body>
 </html>
